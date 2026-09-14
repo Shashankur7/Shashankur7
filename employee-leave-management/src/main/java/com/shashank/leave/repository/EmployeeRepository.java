a2ed1b50029f429bedab5103b13a210ca27854f4
Shashank  Bhoyar <bhoyarshashank4@gmail.com>
2026-09-14 20:25:38 +0530

    Add employee repository

---
+++ employee-leave-management/src/main/java/com/shashank/leave/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.shashank.leave.repository;
+
+import com.shashank.leave.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
